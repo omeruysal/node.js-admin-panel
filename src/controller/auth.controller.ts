2806fdca48016eeac2f625dc6ea5dd1d7c1d58ec
Omer <teouysal@outlook.com>
2022-01-08 18:41:21 +0300

    auth competed

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -43,7 +43,7 @@ export const Login = async (req: Request, res: Response) => {
   const payload = {
     id: user.id,
   };
-  const token = sign(payload, 'secret');
+  const token = sign(payload, process.env.SECRET_KEY);
   // We send token in cookie and it makes much secure
   res.cookie('jwt', token, {
     httpOnly: true, // To avoid Xss attacks. No one can reach our cookie with using js
@@ -56,24 +56,8 @@ export const Login = async (req: Request, res: Response) => {
 };
 
 export const AuthenticatedUser = async (req: Request, res: Response) => {
-  try {
-    const jwt = req.cookies['jwt'];
-
-    const payload: any = verify(jwt, 'secret');
-
-    if (!payload) {
-      return res.status(401).json({
-        message: 'unauthenticated',
-      });
-    }
-    const repository = getManager().getRepository(User);
-    const { password, ...user } = await repository.findOne(payload.id);
-    res.send(user);
-  } catch (error) {
-    res.status(401).json({
-      message: 'unauthenticated',
-    });
-  }
+  const { password, ...user } = req['user'];
+  res.send(user);
 };
 
 export const Logout = (req: Request, res: Response) => {
@@ -82,4 +66,34 @@ export const Logout = (req: Request, res: Response) => {
   res.send({
     message: 'success',
   });
+};
+
+export const UpdateInfo = async (req: Request, res: Response) => {
+  try {
+    const user = req['user'];
+    const repository = getManager().getRepository(User);
+    await repository.update(user.id, req.body);
+    const { password, ...data } = await repository.findOne(user.id);
+    res.send(data);
+  } catch (error) {
+    res.status(400);
+  }
+};
+
+export const UpdatePassword = async (req: Request, res: Response) => {
+  try {
+    const user = req['user'];
+    if (req.body.password !== req.body.passwordConfirm) {
+      return res.status(400).json({
+        message: 'Passwords do not match',
+      });
+    }
+    const repository = getManager().getRepository(User);
+    await repository.update(user.id, { password: await bcryptjs.hash(req.body.password, 10) });
+
+    const { password, ...data } = user;
+    res.send(data);
+  } catch (error) {
+    res.status(400);
+  }
 };
---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { getManager } from 'typeorm';
+import { User } from '../entity/user.entity';
+
+export const AuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const jwt = req.cookies['jwt'];
+
+    const payload: any = verify(jwt, process.env.SECRET_KEY);
+
+    if (!payload) {
+      return res.status(401).json({
+        message: 'unauthenticated',
+      });
+    }
+    const repository = getManager().getRepository(User);
+    req['user'] = await repository.findOne(payload.id);
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'unauthenticated',
+    });
+  }
+};
