aba94187f16205d5499876e8d810aec816575d88
Omer <teouysal@outlook.com>
2022-01-15 01:05:54 +0300

    added browser check

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,14 +2,30 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import { getManager } from 'typeorm';
 import { User } from '../entity/user.entity';
+declare module 'express-session' {
+  interface SessionData {
+    browser: string;
+  }
+}
 
 export const AuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    console.log('user browser : ' + req.session.browser);
+    //Cookieden jwt token cekilir
     const jwt = req.cookies['jwt'];
 
+    //jwt saglamasi yapilir
     const payload: any = verify(jwt, process.env.SECRET_KEY);
 
+    //Eger payload yoksa jwt yok demektir ve unauthenticated uyarisi doneriz
     if (!payload) {
+      return res.status(401).json({
+        message: 'unauthenticated',
+      });
+    }
+    //Sessionda jwt var, fakat browser bilgileri ayni degil ise :
+    // Sessionda browser bilgisi var ve requestten gelen browser bilgisi ile sessiondaki browser bilgisi ayni degil ise, unauthenticated uyarisi doneriz
+    if (req.session.browser !== undefined && req.session.browser !== req.headers['user-agent']) {
       return res.status(401).json({
         message: 'unauthenticated',
       });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,11 +4,16 @@ import { Users } from '../controller/user.controller';
 import { AuthMiddleware } from '../middleware/auth.middleware';
 
 export const routes = (router: Router) => {
+  router.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'health check',
+    });
+  });
   router.post('/api/register', Register);
   router.post('/api/login', Login);
   router.get('/api/user', AuthMiddleware, AuthenticatedUser);
   router.post('/api/logout', AuthMiddleware, Logout);
   router.put('/api/user/info', AuthMiddleware, UpdateInfo);
   router.put('/api/user/password', AuthMiddleware, UpdatePassword);
-  router.get('/api/users', Users);
+  router.get('/api/users', AuthMiddleware, Users);
 };
